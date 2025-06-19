a92fec71cba571fef44088ca1cbb84d194d6640a
Wes Vaske <wvaske@micron.com>
2025-06-19 12:20:46 -0600

    Added multi-run verification for submissions. Reporting correctly prints overall submission results. Verifies number of checkpoints done

--- mlpstorage/reporting.py
+++ mlpstorage/reporting.py
@@ -8,12 +8,13 @@ from dataclasses import dataclass
 from typing import List, Dict, Any
 
 from mlpstorage.mlps_logging import setup_logging, apply_logging_options
-from mlpstorage.config import MLPS_DEBUG, BENCHMARK_TYPES, EXIT_CODE, PARAM_VALIDATION, LLM_MODELS, MODELS
-from mlpstorage.rules import get_runs_files, BenchmarkRunVerifier, BenchmarkRun, Issue
+from mlpstorage.config import MLPS_DEBUG, BENCHMARK_TYPES, EXIT_CODE, PARAM_VALIDATION, LLM_MODELS, MODELS, ACCELERATORS
+from mlpstorage.rules import get_runs_files, BenchmarkVerifier, BenchmarkRun, Issue
 from mlpstorage.utils import flatten_nested_dict, remove_nan_values
 
 @dataclass
 class Result:
+    multi: bool
     benchmark_type: BENCHMARK_TYPES
     benchmark_command: str
     benchmark_model: [LLM_MODELS, MODELS]
@@ -44,17 +45,18 @@ class ReportGenerator:
             self.logger.error(f'Results directory {self.results_dir} does not exist')
             sys.exit(EXIT_CODE.FILE_NOT_FOUND)
 
-        self.results = []
+        self.run_results = dict()           # {run_id : result_dict }
+        self.workload_results = dict()      # {(model) | (model, accelerator) : result_dict }
         self.accumulate_results()
         self.print_results()
 
     def generate_reports(self):
         # Verify the results directory exists:
         self.logger.info(f'Generating reports for {self.results_dir}')
-        results_dicts = [report.benchmark_run.as_dict() for report in self.results]
+        run_result_dicts = [report.benchmark_run.as_dict() for report in self.run_results.values()]
 
-        self.write_csv_file(results_dicts)
-        self.write_json_file(results_dicts)
+        self.write_csv_file(run_result_dicts)
+        self.write_json_file(run_result_dicts)
             
         return EXIT_CODE.SUCCESS
 
@@ -66,16 +68,16 @@ class ReportGenerator:
         If summary.json files does not exist, set status=Failed and only use data from metadata.json the run_info from the result_files dictionary
         :return:
         """
-        results = []
         benchmark_runs = get_runs_files(self.results_dir, logger=self.logger)
 
         self.logger.info(f'Accumulating results from {len(benchmark_runs)} runs')
         for benchmark_run in benchmark_runs:
             self.logger.ridiculous(f'Processing run: \n{pprint.pformat(benchmark_run)}')
-            verifier = BenchmarkRunVerifier(benchmark_run, logger=self.logger)
+            verifier = BenchmarkVerifier(benchmark_run, logger=self.logger)
             category = verifier.verify()
             issues = verifier.issues
             result_dict = dict(
+                multi=False,
                 benchmark_run=benchmark_run,
                 benchmark_type=benchmark_run.benchmark_type,
                 benchmark_command=benchmark_run.command,
@@ -84,13 +86,45 @@ class ReportGenerator:
                 category=category,
                 metrics=benchmark_run.metrics
             )
-            self.results.append(Result(**result_dict))
+            self.run_results[benchmark_run.run_id] = Result(**result_dict)
+
+        # Group runs for workload to run additional verifiers
+        # These will be manually defined as these checks align with a specific submission version
+        # I need to group by model. For training workloads we also group by accelerator but the same checker
+        # is used based on model.
+        workload_runs = dict()
+
+        for benchmark_run in benchmark_runs:
+            workload_key = (benchmark_run.model, benchmark_run.accelerator)
+            if workload_key not in workload_runs.keys():
+                workload_runs[workload_key] = []
+            workload_runs[workload_key].append(benchmark_run)
+
+        for workload_key, runs in workload_runs.items():
+            model, accelerator = workload_key
+            if not runs:
+                continue
+            self.logger.info(f'Running additional verifiers for model: {model}, accelerator: {accelerator}')
+            verifier = BenchmarkVerifier(*runs, logger=self.logger)
+            category = verifier.verify()
+            issues = verifier.issues
+            result_dict = dict(
+                multi=True,
+                benchmark_run=runs,
+                benchmark_type=runs[0].benchmark_type,
+                benchmark_command=runs[0].command,
+                benchmark_model=runs[0].model,
+                issues=issues,
+                category=category,
+                metrics=dict()      # Add function to aggregate metrics
+            )
+            self.workload_results[workload_key] = Result(**result_dict)
 
     def print_results(self):
         print("\n========================= Results Report =========================")
         for category in [PARAM_VALIDATION.CLOSED, PARAM_VALIDATION.OPEN, PARAM_VALIDATION.INVALID]:
             print(f"\n------------------------- {category.value.upper()} Report -------------------------")
-            for result in self.results:
+            for result in self.run_results.values():
                 if result.category == category:
                     print(f'\tRunID: {result.benchmark_run.run_id}')
                     print(f'\t    Benchmark Type: {result.benchmark_type.value}')
@@ -120,6 +154,39 @@ class ReportGenerator:
                                 print(f'\t\t- {metric}: {value}')
 
                     print("\n")
+
+        print("\n========================= Submissions Report =========================")
+        for category in [PARAM_VALIDATION.CLOSED, PARAM_VALIDATION.OPEN, PARAM_VALIDATION.INVALID]:
+            print(f"\n------------------------- {category.value.upper()} Report -------------------------")
+            for workload_key, workload_result in self.workload_results.items():
+                if workload_result.category == category:
+                    if workload_result.benchmark_model in LLM_MODELS:
+                        workload_id = f"Checkpointing - {workload_result.benchmark_model}"
+                    elif workload_result.benchmark_model in MODELS:
+                        accelerator = workload_result.benchmark_run[0].accelerator
+                        workload_id = (f"Training - {workload_result.benchmark_model}, "
+                                       f"Accelerator: {accelerator}")
+                    else:
+                        print(f'Unknown workload type: {workload_result.benchmark_model}')
+
+                    print(f'\tWorkloadID: {workload_id}')
+                    print(f'\t    Benchmark Type: {workload_result.benchmark_type.value}')
+                    if workload_result.benchmark_command:
+                        print(f'\t    Command: {workload_result.benchmark_command}')
+
+                    print(f'\t    Runs: ')
+                    for run in workload_result.benchmark_run:
+                        print(f'\t\t- {run.run_id} - [{self.run_results[run.run_id].category.value.upper()}]')
+
+                    if workload_result.issues:
+                        print(f'\t    Issues:')
+                        for issue in workload_result.issues:
+                            print(f'\t\t- {issue}')
+                    else:
+                        print(f'\t\t- No issues found')
+
+                    print("\n")
+
 
     def write_json_file(self, results):
         json_file = os.path.join(self.results_dir,'results.json')
